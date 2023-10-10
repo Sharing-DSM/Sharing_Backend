d914554fb94b6dc16f559d4dc61c089b79026c10
주영 <jyk102999@dsm.hs.kr>
2023-10-10 09:50:20 +0900

    📑 :: Feed Entity

--- src/main/kotlin/com/example/sharing/domain/user/domain/User.kt
+++ src/main/kotlin/com/example/sharing/domain/user/domain/User.kt
@@ -1,10 +1,12 @@
 package com.example.sharing.domain.user.domain
 
 import org.hibernate.annotations.DynamicInsert
+import org.hibernate.annotations.GenericGenerator
 import org.hibernate.validator.constraints.Length
 import java.util.*
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.validation.constraints.NotNull
 
@@ -12,6 +14,8 @@ import javax.validation.constraints.NotNull
 @DynamicInsert
 class User(
     @Id
+    @GeneratedValue(generator = "uuid4")
+    @GenericGenerator(name = "uuid4", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(columnDefinition = "BINARY(16)")
     val id: UUID,
 
