45181857d9f726e7bc48ce35377f1fc31307d446
jyk1029 <jyk102999@gmail.com>
2023-10-15 04:04:47 +0900

    ⚡️ :: 게시글 작성 api 구현

--- src/main/kotlin/com/example/sharing/domain/feed/presentation/FeedController.kt
+++ src/main/kotlin/com/example/sharing/domain/feed/presentation/FeedController.kt
@@ -1,6 +1,7 @@
 package com.example.sharing.domain.feed.presentation
 
 import com.example.sharing.domain.feed.presentation.dto.request.CreateFeedRequest
+<<<<<<< main
 import com.example.sharing.domain.feed.presentation.dto.request.QueryAddressRequest
 import com.example.sharing.domain.feed.presentation.dto.request.UpdateFeedRequest
 import com.example.sharing.domain.feed.presentation.dto.response.QueryAddressResponse
@@ -12,11 +13,16 @@ import org.springframework.http.HttpStatus.*
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
+=======
+import com.example.sharing.domain.feed.service.CreateFeedService
+import org.springframework.http.HttpStatus.*
+>>>>>>> ⚡️ :: 게시글 작성 api 구현
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+<<<<<<< main
 import java.util.*
 import javax.validation.Valid
 
@@ -28,11 +34,23 @@ class FeedController(
   private val searchAddressService: SearchAddressService,
 ) {
     
+=======
+import javax.validation.Valid
+
+@RequestMapping("/feed")
+@RestController
+class FeedController(
+
+    private val createFeedService: CreateFeedService,
+) {
+
+>>>>>>> ⚡️ :: 게시글 작성 api 구현
     @ResponseStatus(CREATED)
     @PostMapping
     fun createFeed(@RequestBody @Valid request: CreateFeedRequest) {
         createFeedService.execute(request)
     }
+<<<<<<< main
     
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/{feed-id}")
@@ -44,4 +62,6 @@ class FeedController(
     fun getAddress(@RequestBody request: QueryAddressRequest): QueryAddressResponse {
         return searchAddressService.execute(request)
     }
+=======
+>>>>>>> ⚡️ :: 게시글 작성 api 구현
 }
