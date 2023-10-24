7f42345342e0cca448cd94a6c206d5d3956cc37d
sim <simyechan87@gmail.com>
2023-10-24 19:37:42 +0900

    ⚡️ :: 게시글 삭제 api 구현

--- src/main/kotlin/com/example/sharing/domain/feed/presentation/FeedController.kt
+++ src/main/kotlin/com/example/sharing/domain/feed/presentation/FeedController.kt
@@ -1,6 +1,7 @@
 package com.example.sharing.domain.feed.presentation
 
 import com.example.sharing.domain.feed.presentation.dto.request.CreateFeedRequest
+<<<<<<< main
 import com.example.sharing.domain.feed.presentation.dto.request.QueryAddressRequest
 import com.example.sharing.domain.feed.presentation.dto.request.UpdateFeedRequest
 import com.example.sharing.domain.feed.presentation.dto.response.FeedElement
@@ -27,10 +28,29 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 import javax.validation.Valid
+=======
+import com.example.sharing.domain.feed.service.CreateFeedService
+import com.example.sharing.domain.feed.presentation.dto.request.QueryAddressRequest
+import com.example.sharing.domain.feed.presentation.dto.response.QueryAddressResponse
+import com.example.sharing.domain.feed.service.DeleteFeedService
+import com.example.sharing.domain.feed.service.SearchAddressService
+import org.springframework.http.HttpStatus.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import java.util.*
+>>>>>>> ⚡️ :: 게시글 삭제 api 구현
 
 @RequestMapping("/feeds")
 @RestController
 class FeedController(
+<<<<<<< main
   private val updateFeedService: UpdateFeedService,
   private val createFeedService: CreateFeedService,
   private val deleteFeedService: DeleteFeedService,
@@ -40,18 +60,30 @@ class FeedController(
   private val queryFeedByInterestAreaService: QueryFeedByInterestAreaService,
   private val queryFeedListByMapService: QueryFeedListByMapService
 ) {
+=======
+  private val createFeedService: CreateFeedService,
+  private val deleteFeedService: DeleteFeedService,
+  private val searchAddressService: SearchAddressService,
+) {
+
+>>>>>>> ⚡️ :: 게시글 삭제 api 구현
     @ResponseStatus(CREATED)
     @PostMapping
     fun createFeed(@RequestBody @Valid request: CreateFeedRequest) {
         createFeedService.execute(request)
     }
+<<<<<<< main
     
+=======
+
+>>>>>>> ⚡️ :: 게시글 삭제 api 구현
     @ResponseStatus(NO_CONTENT)
     @DeleteMapping("/{feed-id}")
     fun deleteFeed(@PathVariable("feed-id") id: UUID) {
         deleteFeedService.execute(id)
     }
     
+<<<<<<< main
     @ResponseStatus(NO_CONTENT)
     @PatchMapping("/{feed-id}")
     fun updateFeed(@PathVariable ("feed-id") feedId: UUID, @RequestBody @Valid request: UpdateFeedRequest) {
@@ -83,3 +115,10 @@ class FeedController(
         return queryFeedListByMapService.execute(location)
     }
 }
+=======
+    @GetMapping("/address")
+    fun getAddress(@RequestBody request: QueryAddressRequest): QueryAddressResponse {
+        return searchAddressService.execute(request)
+    }
+}
+>>>>>>> ⚡️ :: 게시글 삭제 api 구현
