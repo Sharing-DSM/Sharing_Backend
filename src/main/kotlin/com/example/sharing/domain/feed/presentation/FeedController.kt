5e3a8219d574fdf3ccdc7b7eaf5ff11749bab868
jyk1029 <jyk102999@gmail.com>
2023-10-24 16:18:52 +0900

    ♻️ :: url 수정

--- src/main/kotlin/com/example/sharing/domain/feed/presentation/FeedController.kt
+++ src/main/kotlin/com/example/sharing/domain/feed/presentation/FeedController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 import org.springframework.web.bind.annotation.GetMapping
 
-@RequestMapping("/feed")
+@RequestMapping("/feeds")
 @RestController
 class FeedController(
   private val createFeedService: CreateFeedService,
