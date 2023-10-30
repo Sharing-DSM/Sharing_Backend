869bee7c3fba828f9a7933b4f9032a19a7d0bf86
주영 <jyk102999@dsm.hs.kr>
2023-10-30 20:13:07 +0900

    ⚡️ :: 인기 게시글 조회

--- src/main/kotlin/com/example/sharing/domain/feed/domain/repository/FeedRepository.kt
+++ src/main/kotlin/com/example/sharing/domain/feed/domain/repository/FeedRepository.kt
@@ -1,9 +1,12 @@
 package com.example.sharing.domain.feed.domain.repository
 
 import com.example.sharing.domain.feed.domain.Feed
+import com.example.sharing.domain.feed.presentation.dto.response.FeedElement
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.util.UUID
+import java.util.*
 
 @Repository
-interface FeedRepository: JpaRepository<Feed, UUID>
+interface FeedRepository: JpaRepository<Feed, UUID> {
+    fun findAllByOrderByViewsDesc(): List<FeedElement>
+}
--- src/main/kotlin/com/example/sharing/domain/feed/presentation/FeedController.kt
+++ src/main/kotlin/com/example/sharing/domain/feed/presentation/FeedController.kt
@@ -3,9 +3,11 @@ package com.example.sharing.domain.feed.presentation
 import com.example.sharing.domain.feed.presentation.dto.request.CreateFeedRequest
 import com.example.sharing.domain.feed.presentation.dto.request.QueryAddressRequest
 import com.example.sharing.domain.feed.presentation.dto.request.UpdateFeedRequest
+import com.example.sharing.domain.feed.presentation.dto.response.FeedElement
 import com.example.sharing.domain.feed.presentation.dto.response.QueryAddressResponse
 import com.example.sharing.domain.feed.presentation.dto.response.QueryFeedDetailResponse
 import com.example.sharing.domain.feed.service.CreateFeedService
+import com.example.sharing.domain.feed.service.QueryFeedByViewsService
 import com.example.sharing.domain.feed.service.QueryFeedDetailService
 import com.example.sharing.domain.feed.service.SearchAddressService
 import com.example.sharing.domain.feed.service.UpdateFeedService
@@ -28,6 +30,7 @@ class FeedController(
   private val createFeedService: CreateFeedService,
   private val searchAddressService: SearchAddressService,
   private val queryFeedDetailService: QueryFeedDetailService,
+  private val queryFeedByViewsService: QueryFeedByViewsService,
 ) {
     @ResponseStatus(CREATED)
     @PostMapping
@@ -49,5 +52,10 @@ class FeedController(
     @GetMapping("/{feed-id}")
     fun getFeedDetail(@PathVariable ("feed-id") feedId: UUID): QueryFeedDetailResponse {
         return queryFeedDetailService.execute(feedId)
+    }
+
+    @GetMapping
+    fun getFeedByViews(): List<FeedElement> {
+        return queryFeedByViewsService.execute()
     }
 }
