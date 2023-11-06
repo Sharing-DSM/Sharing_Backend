70dae996db400f9c3b47818c3c00d7701607b18b
sim <simyechan87@gmail.com>
2023-11-07 08:58:06 +0900

    ⚡️ :: 자원봉사 일정 삭제 구현

--- src/main/kotlin/com/example/sharing/domain/schedule/presentation/ScheduleController.kt
+++ src/main/kotlin/com/example/sharing/domain/schedule/presentation/ScheduleController.kt
@@ -1,12 +1,12 @@
 package com.example.sharing.domain.schedule.presentation
 
-import com.example.sharing.domain.feed.presentation.dto.request.UpdateFeedRequest
 import com.example.sharing.domain.schedule.presentation.dto.request.CreateScheduleRequest
 import com.example.sharing.domain.schedule.presentation.dto.request.UpdateScheduleRequest
 import com.example.sharing.domain.schedule.service.CreateScheduleService
+import com.example.sharing.domain.schedule.service.DeleteScheduleService
 import com.example.sharing.domain.schedule.service.UpdateScheduleService
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import org.springframework.http.HttpStatus.*
 import java.util.*
 import javax.validation.Valid
 
@@ -15,16 +15,23 @@ import javax.validation.Valid
 class ScheduleController (
     private val createScheduleService: CreateScheduleService,
     private val updateScheduleService: UpdateScheduleService,
+    private val deleteScheduleService: DeleteScheduleService,
 ) {
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(CREATED)
     @PostMapping
     fun createSchedule(@RequestBody @Valid request: CreateScheduleRequest) {
         createScheduleService.execute(request)
     }
 
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseStatus(NO_CONTENT)
     @PatchMapping("/{schedule-id}")
-    fun updateFeed(@PathVariable ("schedule-id") scheduleId: UUID, @RequestBody @Valid request: UpdateScheduleRequest) {
+    fun updateSchedule(@PathVariable ("schedule-id") scheduleId: UUID, @RequestBody @Valid request: UpdateScheduleRequest) {
         updateScheduleService.execute(scheduleId, request)
+    }
+
+    @ResponseStatus(NO_CONTENT)
+    @DeleteMapping("/{schedule-id}")
+    fun deleteSchedule(@PathVariable("schedule-id") id: UUID) {
+        deleteScheduleService.execute(id)
     }
 }
