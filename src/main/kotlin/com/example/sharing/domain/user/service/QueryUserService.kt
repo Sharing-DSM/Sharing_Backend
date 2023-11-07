ca2d0a76b1b1be44f3d607a969a7b988af24446a
jyk1029 <jyk102999@gmail.com>
2023-11-07 22:28:04 +0900

    ⚡️ :: 유저 정보 조회 APi 구현

---
+++ src/main/kotlin/com/example/sharing/domain/user/service/QueryUserService.kt
@@ -0,0 +1,19 @@
+package com.example.sharing.domain.user.service
+
+import com.example.sharing.domain.user.domain.repository.UserRepository
+import com.example.sharing.domain.user.facade.UserFacade
+import com.example.sharing.domain.user.presentation.dto.response.QueryUserResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class QueryUserService(
+    private val userFacade: UserFacade,
+    private val userRepository: UserRepository,
+) {
+    @Transactional(readOnly = true)
+    fun execute(): QueryUserResponse {
+        val user = userFacade.getCurrentUser()
+        return QueryUserResponse(user.name, user.accountId)
+    }
+}
