97c2c0f3c98c6d31f2f1634d3c5b09d6d27c3400
김주영 <103026521+jyk1029@users.noreply.github.com>
2023-11-07 23:04:58 +0900

    🔀 :: (#37) 유저 정보 조회 APi 구현 (#38)
    ⚡️ :: 유저 정보 조회 APi 구현

--- src/main/kotlin/com/example/sharing/domain/user/presentation/UserController.kt
+++ src/main/kotlin/com/example/sharing/domain/user/presentation/UserController.kt
@@ -2,7 +2,9 @@ package com.example.sharing.domain.user.presentation
 
 import com.example.sharing.domain.user.presentation.dto.request.UserLoginRequest
 import com.example.sharing.domain.user.presentation.dto.request.UserSignUpRequest
+import com.example.sharing.domain.user.presentation.dto.response.QueryUserResponse
 import com.example.sharing.domain.user.presentation.dto.response.TokenResponse
+import com.example.sharing.domain.user.service.QueryUserService
 import com.example.sharing.domain.user.service.SetInterestAreaService
 import com.example.sharing.domain.user.service.UserLoginService
 import com.example.sharing.domain.user.service.UserSignUpService
@@ -16,6 +18,7 @@ class UserController(
     private val userSignUpService: UserSignUpService,
     private val userLoginService: UserLoginService,
     private val setInterestAreaService: SetInterestAreaService,
+    private val queryUserService: QueryUserService,
 ) {
     @ResponseStatus(CREATED)
     @PostMapping("/signup")
@@ -33,4 +36,7 @@ class UserController(
     fun setInterestArea(@RequestParam("interest-area") interestArea: String) {
         setInterestAreaService.execute(interestArea)
     }
+
+    @GetMapping
+    fun queryUser(): QueryUserResponse = queryUserService.execute()
 }
---
+++ src/main/kotlin/com/example/sharing/domain/user/presentation/dto/response/QueryUserResponse.kt
@@ -0,0 +1,6 @@
+package com.example.sharing.domain.user.presentation.dto.response
+
+data class QueryUserResponse(
+    val name: String,
+    val accountId: String,
+)
