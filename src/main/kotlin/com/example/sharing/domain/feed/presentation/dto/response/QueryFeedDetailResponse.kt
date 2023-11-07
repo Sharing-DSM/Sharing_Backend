376dd0b7022bd97e94d7d47494a3bb1f4db2ec0a
sim <simyechan87@gmail.com>
2023-11-07 16:37:35 +0900

    ♻️ :: 컨플릭트 해결

--- src/main/kotlin/com/example/sharing/domain/feed/presentation/dto/response/QueryFeedDetailResponse.kt
+++ src/main/kotlin/com/example/sharing/domain/feed/presentation/dto/response/QueryFeedDetailResponse.kt
@@ -2,7 +2,11 @@ package com.example.sharing.domain.feed.presentation.dto.response
 
 import java.util.*
 
+<<<<<<< main
 data class QueryFeedDetailResponse(
+=======
+data class QueryFeedDetailResponse (
+>>>>>>> ♻️ :: 컨플릭트 해결
     val feedId: UUID,
     val title: String,
     val addressName: String,
