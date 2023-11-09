023d1fb02dab51fa03c54863eea5a2019c5b6656
심예찬 <101486323+simyechan@users.noreply.github.com>
2023-11-09 15:18:07 +0900

    ⚡️ :: 자원봉사 일정 추가 구현 (#30)
    * ⚡️ :: 게시글 삭제 api 구현
    
    * ⚡️ :: 자원봉사 일정 추가 구현
    
    * ♻️ :: LocalDate 사용
    
    * ♻️ :: refactoring
    
    ---------
    
    Co-authored-by: 김주영 <103026521+jyk1029@users.noreply.github.com>
    Co-authored-by: jyk1029 <jyk102999@gmail.com>

---
+++ src/main/kotlin/com/example/sharing/domain/schedule/domain/Schedule.kt
@@ -0,0 +1,40 @@
+package com.example.sharing.domain.schedule.domain
+
+import com.example.sharing.domain.user.domain.User
+import org.hibernate.annotations.DynamicInsert
+import org.hibernate.annotations.GenericGenerator
+import org.hibernate.validator.constraints.Length
+import java.time.LocalDate
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.FetchType.*
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.validation.constraints.NotNull
+
+@Entity(name = "tbl_schedule")
+@DynamicInsert
+class Schedule (
+    @Id
+    @GeneratedValue(generator = "uuid4")
+    @GenericGenerator(name = "uuid4", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(columnDefinition = "BINARY(16)")
+    val id: UUID,
+
+    @field:NotNull
+    @field:Length(max = 10)
+    var title: String,
+
+    @field:NotNull
+    var date: LocalDate,
+
+    user: User
+) {
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id", columnDefinition = "BINARY(16)", nullable = false)
+    var user = user
+        protected  set
+}
---
+++ src/main/kotlin/com/example/sharing/domain/schedule/presentation/dto/request/CreateScheduleRequest.kt
@@ -0,0 +1,15 @@
+package com.example.sharing.domain.schedule.presentation.dto.request
+
+import java.time.LocalDate
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
+data class CreateScheduleRequest (
+    @field:NotBlank(message = "title는 Null, 공백을 허용하지 않습니다.")
+    @field:Size(max = 10, message = "title는 10자 이하여야 합니다.")
+    val title: String,
+
+    @field:NotNull
+    val date: LocalDate
+)
