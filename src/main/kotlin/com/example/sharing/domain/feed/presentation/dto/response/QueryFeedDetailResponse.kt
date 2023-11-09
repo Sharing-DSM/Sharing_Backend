8f397f380449195fb738c0c9e5c12d0f3aee7b15
주영 <jyk102999@dsm.hs.kr>
2023-11-09 15:00:14 +0900

    ♻️ :: 컨플릭트 해결

--- src/main/kotlin/com/example/sharing/domain/feed/presentation/dto/response/QueryFeedDetailResponse.kt
+++ src/main/kotlin/com/example/sharing/domain/feed/presentation/dto/response/QueryFeedDetailResponse.kt
@@ -2,15 +2,7 @@ package com.example.sharing.domain.feed.presentation.dto.response
 
 import java.util.*
 
-<<<<<<< main
-<<<<<<< main
 data class QueryFeedDetailResponse(
-=======
-data class QueryFeedDetailResponse (
->>>>>>> ♻️ :: 컨플릭트 해결
-=======
-data class QueryFeedDetailResponse(
->>>>>>> ♻️ :: 컨플릭트 해결
     val feedId: UUID,
     val title: String,
     val addressName: String,
