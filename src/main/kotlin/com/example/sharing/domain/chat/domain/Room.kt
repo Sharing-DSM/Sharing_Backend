1e2c61baa066f5ba03667eec3394c2aee4cda7a7
jyk1029 <jyk102999@gmail.com>
2023-11-12 02:47:28 +0900

    🐛 :: Enum 이상한 문제 해결

--- src/main/kotlin/com/example/sharing/domain/chat/domain/Room.kt
+++ src/main/kotlin/com/example/sharing/domain/chat/domain/Room.kt
@@ -7,8 +7,11 @@ import java.time.LocalDateTime
 import java.util.*
 import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.EnumType.*
+import javax.persistence.Enumerated
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.validation.constraints.NotNull
 
 @Entity(name = "tbl_room")
 @DynamicInsert
@@ -19,14 +22,18 @@ class Room(
     @Column(columnDefinition = "BINARY(16)")
     val id: UUID,
 
-    @Column(nullable = false)
-    var roomType: RoomType,
+    roomType: RoomType,
 
     var lastText: String,
 
     @Column(columnDefinition = "DATETIME")
     var lastSendAt: LocalDateTime,
 ) {
+
+    @field:Enumerated(STRING)
+    @field:NotNull
+    var roomType: RoomType = roomType
+        protected set
     fun updateLastTextAndLastSendAt(lastText: String, lastSendAt: LocalDateTime) {
         this.lastText = lastText
         this.lastSendAt = lastSendAt
