7ca5a6b021fc426a95f9af3dd2587e23b84e7602
jyk1029 <jyk102999@gmail.com>
2023-11-13 08:41:19 +0900

    ♻️ :: nullable 제거

--- src/main/kotlin/com/example/sharing/domain/user/domain/User.kt
+++ src/main/kotlin/com/example/sharing/domain/user/domain/User.kt
@@ -39,7 +39,7 @@ class User(
     @field:Length(max = 2000)
     var profile: String?,
 ) {
-    fun updateInterestArea(interestArea: String?) {
+    fun updateInterestArea(interestArea: String) {
         this.interestArea = interestArea
     }
 
