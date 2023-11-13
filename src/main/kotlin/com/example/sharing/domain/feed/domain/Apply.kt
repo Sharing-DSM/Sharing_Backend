ab931a078aa2d7a702a6f9d53ceae45047801458
jyk1029 <jyk102999@gmail.com>
2023-11-13 23:06:50 +0900

    🐛 :: 500 해결

--- src/main/kotlin/com/example/sharing/domain/feed/domain/Apply.kt
+++ src/main/kotlin/com/example/sharing/domain/feed/domain/Apply.kt
@@ -33,8 +33,10 @@ class Apply (
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", columnDefinition = "BINARY(16)", nullable = false)
     var user = user
+        protected set
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "feed_id", columnDefinition = "BINARY(16)", nullable = false)
     var feed = feed
+        protected set
 }
--- src/main/kotlin/com/example/sharing/domain/feed/presentation/dto/response/ApplyElement.kt
+++ src/main/kotlin/com/example/sharing/domain/feed/presentation/dto/response/ApplyElement.kt
@@ -1,11 +1,16 @@
 package com.example.sharing.domain.feed.presentation.dto.response
 
-import com.example.sharing.domain.feed.domain.Feed
+import com.example.sharing.domain.feed.domain.type.Type
 import java.time.LocalDateTime
 import java.util.*
 
 data class ApplyElement(
     val id: UUID,
-    val feed: Feed,
+    val feedId: UUID,
+    val feedTitle: String,
+    val feedAddressName: String,
+    val feedX: Double,
+    val feedY: Double,
+    val feedType: Type,
     val appliedAt: LocalDateTime
 )
