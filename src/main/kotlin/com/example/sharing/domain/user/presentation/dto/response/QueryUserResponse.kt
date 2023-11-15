07cdf88747e2c246954e04f49422cfff94cf7cc1
jyk1029 <jyk102999@gmail.com>
2023-11-15 23:07:11 +0900

    ♻️ :: 유저 정보 프로필도 추가

--- src/main/kotlin/com/example/sharing/domain/user/presentation/dto/response/QueryUserResponse.kt
+++ src/main/kotlin/com/example/sharing/domain/user/presentation/dto/response/QueryUserResponse.kt
@@ -3,5 +3,6 @@ package com.example.sharing.domain.user.presentation.dto.response
 data class QueryUserResponse(
     val name: String,
     val accountId: String,
-    val age: Int
+    val age: Int,
+    val profile: String?
 )
