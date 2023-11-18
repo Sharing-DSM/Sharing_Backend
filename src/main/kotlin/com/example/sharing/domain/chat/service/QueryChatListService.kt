394e35a68db986f28cfdfe4bbaf71d4bc9a12f44
jyk1029 <jyk102999@gmail.com>
2023-11-18 17:06:17 +0900

    ♻️ :: 읽은 시각 업데이트

--- src/main/kotlin/com/example/sharing/domain/chat/service/QueryChatListService.kt
+++ src/main/kotlin/com/example/sharing/domain/chat/service/QueryChatListService.kt
@@ -3,11 +3,13 @@ package com.example.sharing.domain.chat.service
 import com.example.sharing.domain.chat.domain.repository.ChatRepository
 import com.example.sharing.domain.chat.domain.repository.PrivateRoomRepository
 import com.example.sharing.domain.chat.facade.RoomFacade
+import com.example.sharing.domain.chat.facade.RoomUserFacade
 import com.example.sharing.domain.chat.presentation.dto.ChatResponse
 import com.example.sharing.domain.chat.presentation.dto.QueryChatListResponse
 import com.example.sharing.domain.user.facade.UserFacade
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
 import java.util.*
 import java.util.stream.Collectors
 
@@ -17,12 +19,15 @@ class QueryChatListService(
     private val roomFacade: RoomFacade,
     private val privateRoomRepository: PrivateRoomRepository,
     private val chatRepository: ChatRepository,
+    private val roomUserFacade: RoomUserFacade,
 ) {
     @Transactional(readOnly = true)
     fun execute(roomId: UUID): QueryChatListResponse {
         val user = userFacade.getCurrentUser()
         val room = roomFacade.getRoomById(roomId)
-        val roomUser = privateRoomRepository.findByRoomAndUserA(room, user).userB
+        val roomUser = roomUserFacade.getByRoomAndUser(room.id, user.id)
+
+        roomUser.updateLastReadAt(LocalDateTime.now())
 
         return QueryChatListResponse(
             chatRepository.findAllByRoom(room)
@@ -30,8 +35,9 @@ class QueryChatListService(
                 .map { chat ->
                     ChatResponse.of(chat, chat.user.id == user.id)
                 }
+                .sorted()
                 .collect(Collectors.toList()),
-            userName = roomUser.name
+            userName = privateRoomRepository.findByRoomAndUserA(room, user).userB.name
         )
     }
 }
