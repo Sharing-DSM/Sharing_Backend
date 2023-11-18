d76cb2aca72f270f87bd6d4bbc4660e99fa3c644
jyk1029 <jyk102999@gmail.com>
2023-11-18 15:26:24 +0900

    🐛 :: filter 처리

--- src/main/kotlin/com/example/sharing/domain/chat/service/QueryMyRoomListService.kt
+++ src/main/kotlin/com/example/sharing/domain/chat/service/QueryMyRoomListService.kt
@@ -1,10 +1,9 @@
 package com.example.sharing.domain.chat.service
 
 import com.example.sharing.domain.chat.domain.repository.PrivateRoomRepository
-import com.example.sharing.domain.chat.domain.repository.RoomUserRepository
+import com.example.sharing.domain.chat.facade.RoomUserFacade
 import com.example.sharing.domain.chat.presentation.dto.QueryMyRoomListResponse
 import com.example.sharing.domain.chat.presentation.dto.RoomResponse
-import com.example.sharing.domain.user.exception.UserNotFoundException
 import com.example.sharing.domain.user.facade.UserFacade
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -13,7 +12,7 @@ import java.util.stream.Collectors
 @Service
 class QueryMyRoomListService(
     private val userFacade: UserFacade,
-    private val roomUserRepository: RoomUserRepository,
+    private val roomUserFacade: RoomUserFacade,
     private val privateRoomRepository: PrivateRoomRepository,
 ) {
     @Transactional(readOnly = true)
@@ -22,11 +21,12 @@ class QueryMyRoomListService(
         val userBList = privateRoomRepository.findAllByUserA(user)
 
         return QueryMyRoomListResponse(
-            roomUserRepository.findAllByUser(user)
+            roomUserFacade.getByUser(user)
                 .stream()
-                .map { roomUser ->
-                    val matchedUserB = userBList.find { it.userA == roomUser.user || it.userB == roomUser.user }
-                    RoomResponse.of(roomUser, matchedUserB?.userB ?: throw UserNotFoundException.EXCEPTION)
+                .flatMap { roomUser ->
+                    userBList.stream()
+                        .filter { privateRoom -> privateRoom.userA == user && privateRoom.room == roomUser.room }
+                        .map { RoomResponse.of(roomUser, it.userB) }
                 }
                 .collect(Collectors.toList()))
     }
